fabe972dce21179eff0941de9c9243333567c466
RIOSISAAC4 <rios.isaac.401@gmail.com>
2025-11-21 14:05:54 -0600

    Create database.py

---
+++ assets/database.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("OptionsMenu with Dictionary")
+
+options1 = ["Wolves", "Humans", "Shoebill", "Axolotl"]
+options1 = ["Wolves", "Humans", "Shoebill", "Axolotl"]
+
+my_dict = {
+  ("Wolves", "Wolves"): "",
+  ("Wolves", "Humans"): "",
+  ("Wolves", "Shoebill"): "",
+  ("Wolves", "Axolotl"): "",
+  ("Humans", "Humans"): "",
+  ("Humans", "Shoebill"): "",
+  ("Humans", "Axolotl"): "",
+  ("Shoebill", "Shoebills"): "",
